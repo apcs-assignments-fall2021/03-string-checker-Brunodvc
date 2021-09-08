b7099859e4feda4916128659adb701a078f78d91
brunodovalcallao <24bdovalcallao@athenian.org>
2021-09-08 00:26:02 -0700

    first attempt, though you should walk us through how to code the test thingy during class

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,42 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'b' || str.charAt(i) == 'c') {
+                count += 1;
+            }
+        }
+        return count;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
+        for (int i = 0; i < str.length()-3; i++){
+            if (str.substring(i,i+3).equals("The")||str.substring(i,i+3).equals("the")){
+                return true;
+            }
+        }
         return false;
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
+        String result="";
+        for(int i=str.length()-1; i>=0; i--) {
+            result = result + str.charAt(i);
+
+        if (result.equals(str)){
+            return true;
+        }
+        }
         return false;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println(containsThe("the athenian school"));
 
-        // YOUR CODE HERE
     }
 }
